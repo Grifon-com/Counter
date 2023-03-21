d7b9bb7cbef33912cf029a790aae7e736428d67a
Mashuk Grigoriy <mashukgrisha@mail.ru>
2023-03-22 01:17:48 +0300

    Создал счётчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLable: UILabel!
+    private var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func counterIncrease(_ sender: UIButton) {
+        count += 1
+        countLable.text = "\(count)"
+    }
 }
 
